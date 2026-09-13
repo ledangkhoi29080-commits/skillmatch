cffc2411b0d77213a1cda05c48b97b6fb30d0bb8
ledangkhoi29080-commits <ledangkhoi29080@gmail.com>
2026-09-13 10:54:28 +0700

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://iqwgfhrwbvpkggliuuak.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlxd2dmaHJ3YnZwa2dnbGl1dWFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNzgyNDgsImV4cCI6MjEwNDY1NDI0OH0.s86re0aTzRBhiWRLNRl6w0ok9SMNmO6bLm0HDsbzbWE';
+const supabaseUrl = 'https://YOUR_SUPABASE_PROJECT_URL.supabase.co';
+const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
